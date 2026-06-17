a14e27ada6b127a9d96ade4022403f2609db30dc
Jerry Miller <millerjerrykamsi999@gmail.com>
2026-06-17 13:48:41 +0100

    Clean up urls.py by removing comments
    Removed commented lines for sitemap and media URL.

--- shop_project/urls.py
+++ shop_project/urls.py
@@ -8,15 +8,12 @@ sitemaps = {
     'products': ProductSitemap,
 }
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('shop/', views.shop, name='shop'),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('product/<int:product_id>/related/', views.load_more_related, name='load_more_related'),
     path('subscribe/', views.subscribe, name='subscribe'),
-
-    # Add this line for the sitemap
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    
-    # ... your media url if you have it
 ]
